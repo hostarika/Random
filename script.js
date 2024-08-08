a779e973441e5763e801704edcc4fb3ae21f9d17
Muhammaddavron <muhammaddavron123@gmail.com>
2024-08-08 13:10:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+alert('Sizda hozir random sonlar chiqishni boshlaydi')
+
+let a = Math.floor(Math.random() * 80)
+
+let b = a/2
+
+let c = a*2
+
+let d = c%3
+
+alert(' Sizning random soningiz ' + a)
+
+alert( a  + ' ni 2 ga bolsa ' + b + ' chiqadi, 2 ga kopaytirsa ' + c + ' chiqadi ')
+
+alert( c + ' ni 3 ga bolsa '+ d + ' qoldiq boladi')
